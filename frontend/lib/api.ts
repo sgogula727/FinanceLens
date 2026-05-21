044c5fa32077273358e808280c0e55427ca5ddf0
Sai Maanasa Gogula <saimaanasagogula@mac.mynetworksettings.com>
2026-05-20 21:05:38 -0400

    use env var for frontened API Url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,4 @@
-const API_BASE = "http://localhost:8000";
-
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 export async function getTransactions() {
   const res = await fetch(`${API_BASE}/transactions`, { cache: "no-store" });
   return res.json();
